e8eec2f5f5a277b39e8cbd631c63250647fefef1
roma <arfonosdima13@gmail.com>
2023-12-09 21:55:29 +0300

    :dsada

--- labs/lab10/lab09/lab9-4.asm
+++ labs/lab10/lab09/lab9-4.asm
@@ -1,34 +1,39 @@
 %include 'in_out.asm'
 SECTION .data
 msg db "Результат: ",0
-msg2 db "Функция: f(x)=30x-11"
+msg1 db "Функция: f(x)=3(x+2)"
 SECTION .text
 global _start
 _start:
-mov eax,msg2
-call sprintLF
-pop ecx
-pop edx
-sub ecx,1
-mov esi, 0
-mov edi, 30
+pop ecx ; Извлекаем из стека в `ecx` количество
+; аргументов (первое значение в стеке)
+pop edx ; Извлекаем из стека в `edx` имя программы
+; (второе значение в стеке)
+sub ecx,1 ; Уменьшаем `ecx` на 1 (количество
+; аргументов без названия программы)
+mov esi, 0 ; Используем `esi` для хранения
+; промежуточных сумм
 next:
-cmp ecx,0h
-jz _end
-pop eax
-call atoi
+cmp ecx,0h ; проверяем, есть ли еще аргументы
+jz _end ; если аргументов нет выходим из цикла
+; (переход на метку `_end`)
+pop eax ; иначе извлекаем следующий аргумент из стека
+call atoi ; преобразуем символ в число
 call _calcul
-loop next
+; след. аргумент `esi=esi+eax`
+loop next ; переход к обработке следующего аргумента
 _end:
-mov eax, msg
+mov eax,msg1 ;
+call sprintLF ;
+mov eax, msg ; вывод сообщения "Результат: "
 call sprint
-mov eax, esi
-call iprintLF
-call quit
+mov eax, esi ; записываем сумму в регистр `eax`
+call iprintLF ; печать результата
+call quit ; завершение программы
 
 _calcul:
-mul edi
-sub eax,11
-add esi,eax
-
+add eax,2 ;
+mov ebx,3 ;ebx=3
+mul ebx; eax=eax*ebx
+add esi,eax ; добавляем к промежуточной сумме
 ret
